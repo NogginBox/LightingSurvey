b51ba85ddc9cfbc93676641c6eed95c80378a231
Richard Garside <richard.garside@nogginbox.co.uk>
2019-07-19 17:54:29 +0100

    New project

---
+++ CqsLighting.MvcSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CqsLighting.MvcSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
