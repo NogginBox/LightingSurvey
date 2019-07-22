623593a6222233f08fdb6dd60dcec337d8fb26ee
Richard Garside <richard.garside@nogginbox.co.uk>
2019-07-22 22:35:09 +0100

    Installed MarkEmbling.PostcodesIO Nuget package

---
+++ LightingSurvey.MvcSite/Services/Postcodes/PostcodeLookupService.cs
@@ -0,0 +1,14 @@
+﻿using MarkEmbling.PostcodesIO;
+
+namespace LightingSurvey.MvcSite.Services.Postcodes
+{
+    public class PostcodeLookupService
+    {
+        readonly IPostcodesIOClient _postcoder;
+
+        public PostcodeLookupService()
+        {
+            _postcoder = new PostcodesIOClient();
+        }
+    }
+}
