195652a75150f457432361b33d9dd418e950f1fe
Richard Garside <richard.garside@nogginbox.co.uk>
2019-07-23 19:40:42 +0100

    Question 1 unit tests

--- LightingSurvey.Tests/MvcSite/Controllers/SurveyControllerTests.cs
+++ LightingSurvey.Tests/MvcSite/Controllers/SurveyControllerTests.cs
@@ -3,6 +3,7 @@ using LightingSurvey.Data.Models;
 using LightingSurvey.Data.Repositories;
 using LightingSurvey.MvcSite.Controllers;
 using LightingSurvey.MvcSite.Services;
+using LightingSurvey.MvcSite.ViewModels.Survey;
 using Microsoft.AspNetCore.Mvc;
 using NSubstitute;
 using System;
@@ -14,15 +15,17 @@ namespace LightingSurvey.Tests.MvcSite.Controllers
 {
     public class SurveyControllerTests
     {
+        private const string ExcellentName = "Richard Garside";
+        private readonly DateTime FakeNow = new DateTime(2019, 7, 1);
+
         [Fact]
         public async Task TestStartCreatesNewResponseWhenNoCurrentResponseThenRedirects()
         {
             // Arrange
-            var now = new DateTime(2019, 7, 1);
             var clientStorage = Substitute.For<IClientSideStorageService>();
             var dateTime = Substitute.For<IDateTimeService>();
             var repo = Substitute.For<ISurveyResponseRepository>();
-            repo.Create().Returns(SurveyResponse.CreateNew(now));
+            repo.Create().Returns(SurveyResponse.CreateNew(FakeNow));
             var controller = new SurveyController(clientStorage, dateTime, repo);
 
             // Act
@@ -37,12 +40,11 @@ namespace LightingSurvey.Tests.MvcSite.Controllers
         public async Task TestStartDoesNotCreatesNewResponseWhenCurrentResponseFoundThenRedirects()
         {
             // Arrange
-            var now = new DateTime(2019, 7, 1);
             var clientStorage = Substitute.For<IClientSideStorageService>();
             var dateTime = Substitute.For<IDateTimeService>();
             var repo = Substitute.For<ISurveyResponseRepository>();
-            repo.Create().Returns(SurveyResponse.CreateNew(now));
-            repo.Find(Arg.Any<string>()).Returns(SurveyResponse.CreateNew(now.AddDays(-1)));
+            repo.Create().Returns(SurveyResponse.CreateNew(FakeNow));
+            repo.Find(Arg.Any<string>()).Returns(SurveyResponse.CreateNew(FakeNow.AddDays(-1)));
 
             var controller = new SurveyController(clientStorage, dateTime, repo);
 
@@ -52,6 +54,61 @@ namespace LightingSurvey.Tests.MvcSite.Controllers
             // Assert
             await repo.DidNotReceive().Create();
             Assert.IsType<RedirectToActionResult>(result);
+        }
+
+        [Fact]
+        public void TestQuestion1ShowsCurrentName()
+        {
+            // Arrange
+            var response = SurveyResponse.CreateNew(FakeNow);
+            response.Respondent.Name = ExcellentName;
+            var clientStorage = Substitute.For<IClientSideStorageService>();
+            var dateTime = Substitute.For<IDateTimeService>();
+            var repo = Substitute.For<ISurveyResponseRepository>();
+
+            var controller = new SurveyController(clientStorage, dateTime, repo)
+            {
+                CurrentResponse = response
+            };
+
+            // Act
+            var result = controller.Question1();
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
+            var viewResult = result as ViewResult;
+            Assert.IsType<QuestionPageViewModel<string>>(viewResult.Model);
+            var model = viewResult.Model as QuestionPageViewModel<string>;
+            Assert.Equal(ExcellentName, model.Question.Answer);
+        }
+
+        [Fact]
+        public async Task TestQuestion1SavesCurrentName()
+        {
+            // Arrange
+            var response = SurveyResponse.CreateNew(FakeNow);
+            response.Respondent.Name = "Wrong name";
+            var clientStorage = Substitute.For<IClientSideStorageService>();
+            var dateTime = Substitute.For<IDateTimeService>();
+            var repo = Substitute.For<ISurveyResponseRepository>();
+
+            var controller = new SurveyController(clientStorage, dateTime, repo)
+            {
+                CurrentResponse = response
+            };
+
+            var questionAnswer = new NameQuestionViewModel
+            {
+                Answer = ExcellentName
+            };
+
+            // Act
+            var result = await controller.Question1(questionAnswer);
+
+            // Assert
+            await repo.Received().SaveChanges();
+            Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal(ExcellentName, controller.CurrentResponse.Respondent.Name);
         }
     }
 }
