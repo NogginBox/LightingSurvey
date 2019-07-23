8a32ee24edde88b509f36277b5c95f1a883072d2
Richard Garside <richard.garside@nogginbox.co.uk>
2019-07-23 21:34:30 +0100

    PostcodeLookupService can get latlongs, but not street name

---
+++ LightingSurvey.MvcSite/Services/Postcodes/IPostcodeLookupService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using LightingSurvey.MvcSite.Services.Postcodes.Model;
+
+namespace LightingSurvey.MvcSite.Services.Postcodes
+{
+    public interface IPostcodeLookupService
+    {
+        Task<LatLong> Search(string postcode);
+    }
+}
