6b33ed3faee941761cc04293ac09768fad7c94b0
Richard Garside <richard.garside@nogginbox.co.uk>
2019-07-23 17:07:52 +0100

    Survey Start action unit test

---
+++ LightingSurvey.Tests/MvcSite/Controllers/SurveyControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using LightingSurvey.Common.Services;
+using LightingSurvey.Data.Models;
+using LightingSurvey.Data.Repositories;
+using LightingSurvey.MvcSite.Controllers;
+using LightingSurvey.MvcSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LightingSurvey.Tests.MvcSite.Controllers
+{
+    public class SurveyControllerTests
+    {
+        [Fact]
+        public async Task TestStartCreatesNewResponseWhenNoCurrentResponse()
+        {
+            // Arrange
+            var now = new DateTime(2019, 7, 1);
+            var clientStorage = Substitute.For<IClientSideStorageService>();
+            var dateTime = Substitute.For<IDateTimeService>();
+            var repo = Substitute.For<ISurveyResponseRepository>();
+            repo.Create().Returns(SurveyResponse.CreateNew(now));
+            var controller = new SurveyController(clientStorage, dateTime, repo);
+
+            // Act
+            var result = await controller.Start();
+
+            // Assert
+            Assert.IsType<RedirectToActionResult>(result);
+            await repo.Received().Create();
+        }
+    }
+}
